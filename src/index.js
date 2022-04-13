909676f8351582e117cd38021658d36c0bb2297c
Huy <huy.tran@hotmail.com>
2022-04-13 17:14:27 -0400

    removed port for rest api endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import http from 'http';
 
 import logger from './utils/logger';
-import { API_PORT, APOLLO_PORT } from './config';
+import { APOLLO_PORT } from './config';
 import createApolloServer from './apolloServer';
 import app from './app';
 
@@ -14,9 +14,9 @@ const startServer = async () => {
 
   httpServer.on('request', app.callback());
 
-  await new Promise((resolve) =>
-    httpServer.listen({ port: API_PORT }, resolve),
-  );
+  // await new Promise((resolve) =>
+  //   httpServer.listen({ port: API_PORT }, resolve),
+  // );
 
   logger.info(`Apollo Server ready at http://localhost:${APOLLO_PORT}`);
 };
